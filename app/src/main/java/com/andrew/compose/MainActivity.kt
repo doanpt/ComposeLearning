930d401aa339b36cf061271f60cca46ebce8ae39
FPT_DoanPT1 <doanpt93cntt@gmail.com>
2024-04-14 08:13:46 +0700

    Add a button and modify margin/padding/background

--- app/src/main/java/com/andrew/compose/MainActivity.kt
+++ app/src/main/java/com/andrew/compose/MainActivity.kt
@@ -3,16 +3,23 @@ package com.andrew.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -53,9 +60,27 @@ fun ContentView() {
             //     fontSize = 80.sp,
             //     fontWeight = FontWeight.Thin
             // )
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .padding(20.dp)
+                .background(Color.Green)
+                .padding(20.dp)
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        // OutlinedTextField(
+        //     value = "",
+        //     onValueChange = {
+        //         // do something here
+        //     },
+        //     placeholder = {
+        //         Text(text = "Enter a name")
+        //     },
+        //     colors = TextFieldDefaults.colors(
+        //         cursorColor = Color.Blue,
+        //         focusedContainerColor = Color.Red
+        //     ),
+        //     modifier = Modifier
+        //         .fillMaxWidth()
+        //         .padding(horizontal = 20.dp),
+        // )
         TextField(
             value = "",
             onValueChange = {
@@ -64,8 +89,26 @@ fun ContentView() {
             placeholder = {
                 Text(text = "Enter a name")
             },
-            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                cursorColor = Color.Blue,
+                focusedContainerColor = Color.Red
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.DarkGray
+            ),
+            contentPadding = PaddingValues(horizontal = 40.dp),
+        ) {
+            Text(text = "Add a name")
+        }
     }
 }
 
