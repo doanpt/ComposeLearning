9d65bdac083e20b732f4b769e4fade69d38fe53b
FPT_DoanPT1 <doanpt93cntt@gmail.com>
2024-04-14 09:33:53 +0700

    Add navigation drawer

--- app/src/main/java/com/andrew/compose/MainActivity.kt
+++ app/src/main/java/com/andrew/compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andrew.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -26,13 +28,18 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -40,23 +47,28 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.andrew.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.launch
 
 data class BottomNavigationItem(
     val title: String,
@@ -101,67 +113,113 @@ fun MainScreen() {
             unselectedIcon = Icons.Outlined.Person,
         )
     )
-    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text("Compose App", modifier = Modifier.padding(horizontal = 20.dp))
-                },
-                navigationIcon = {
-                    Icon(Icons.Default.Menu, contentDescription = "Menu")
-                },
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
+    val navigationState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(modifier = Modifier.height(26.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .fillMaxWidth()
+                        .align(CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(26.dp))
+                items.forEachIndexed { index, drawerItem ->
+                    NavigationDrawerItem(label = {
+                        Text(text = drawerItem.title)
+                    }, selected = index == selectedItemIndex, onClick = {
+                        selectedItemIndex = index
+                        // TODO navigate here
+                    }, icon = {
                         Icon(
-                            imageVector = Icons.Filled.Settings,
-                            contentDescription = "Localized description"
+                            imageVector = if (index == selectedItemIndex) {
+                                drawerItem.selectedIcon
+                            } else drawerItem.unselectedIcon,
+                            contentDescription = drawerItem.title
                         )
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        selected = selectedItemIndex == index,
-                        onClick = {
-                            selectedItemIndex = index
-                            // TODO navigate here
-                        },
-                        label = {
-                            Text(text = item.title)
-                        },
-                        icon = {
-                            Icon(
-                                imageVector =
-                                if (selectedItemIndex == index)
-                                    item.selectedIcon
-                                else item.unselectedIcon,
-                                contentDescription = item.title
-                            )
-                        }
+                    }, modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
                     )
+
                 }
             }
         },
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Add") },
-                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                onClick = {
+        drawerState = navigationState,
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text("Compose App", modifier = Modifier.padding(horizontal = 20.dp))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                navigationState.open()
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Settings,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                NavigationBar {
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            selected = selectedItemIndex == index,
+                            onClick = {
+                                selectedItemIndex = index
+                                // TODO navigate here
+                            },
+                            label = {
+                                Text(text = item.title)
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector =
+                                    if (selectedItemIndex == index)
+                                        item.selectedIcon
+                                    else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            }
+                        )
+                    }
                 }
-            )
+            },
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Add") },
+                    icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                    onClick = {
+                    }
+                )
+            }
+        ) { paddings ->
+            Box(modifier = Modifier.padding(paddings)) {
+                ContentView()
+            }
         }
-    ) { paddings ->
-        Box(modifier = Modifier.padding(paddings)) {
-            ContentView()
-        }
+
     }
 }
 
@@ -228,7 +286,7 @@ fun ContentView() {
                 names.add(inputName)
                 inputName = ""
             },
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier.align(CenterHorizontally),
             colors = ButtonDefaults.buttonColors(
                 contentColor = Color.White,
                 containerColor = Color.DarkGray
