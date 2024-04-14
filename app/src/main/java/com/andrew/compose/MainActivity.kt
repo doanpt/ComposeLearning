615b0b7361111b41109280312319fc119c876d5f
FPT_DoanPT1 <doanpt93cntt@gmail.com>
2024-04-14 08:20:22 +0700

    Handle state and input, add item to lazy columm

--- app/src/main/java/com/andrew/compose/MainActivity.kt
+++ app/src/main/java/com/andrew/compose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -48,6 +55,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
+    var inputName by remember {
+        mutableStateOf("")
+    }
+    val names = remember {
+        mutableStateListOf("")
+    }
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = "Welcome to Compose",
@@ -82,9 +95,9 @@ fun ContentView() {
         //         .padding(horizontal = 20.dp),
         // )
         TextField(
-            value = "",
+            value = inputName,
             onValueChange = {
-                // do something here
+                inputName = it
             },
             placeholder = {
                 Text(text = "Enter a name")
@@ -99,7 +112,10 @@ fun ContentView() {
         )
         Spacer(modifier = Modifier.height(20.dp))
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                names.add(inputName)
+                inputName = ""
+            },
             modifier = Modifier.align(Alignment.CenterHorizontally),
             colors = ButtonDefaults.buttonColors(
                 contentColor = Color.White,
@@ -108,6 +124,11 @@ fun ContentView() {
             contentPadding = PaddingValues(horizontal = 40.dp),
         ) {
             Text(text = "Add a name")
+        }
+        LazyColumn {
+            items(names) {
+                Text(text = it)
+            }
         }
     }
 }
